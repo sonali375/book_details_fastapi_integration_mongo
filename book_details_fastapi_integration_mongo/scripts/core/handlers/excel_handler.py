759cc0acd62fe837b7458695bc6c43247b09ae89
Sonali Sahu <71460151+sonali375@users.noreply.github.com>
2023-05-30 19:19:40 +0530

    Add files via upload

--- book_details_fastapi_integration_mongo/scripts/core/handlers/excel_handler.py
+++ book_details_fastapi_integration_mongo/scripts/core/handlers/excel_handler.py
@@ -16,27 +16,47 @@ class ExcelGeneration:
     def __init__(self):
         self.mongo_obj = MongoCollectionBaseClass()
 
+    @staticmethod
     def excel_from_billing_data(self):
         """function to generate Excel sheet from billing data"""
         try:
-            logger.info("Handler:excel_from_billing_data")
-            # Fetching the data for the Excel file
-            excel_data = pipeline_aggregation(Aggregation.aggr)
-            # overall_total = sum(row['total_amount'] for row in excel_data)
-            # overall_total_row = {'overall_total': overall_total}
-            # excel_data.append(overall_total_row)
-            print(excel_data)
+            response_list = pipeline_aggregation(Aggregation.aggr)
 
-            # Create a DataFrame from the data
-            df = pd.DataFrame(excel_data)
+            dataframe = pd.DataFrame(data=response_list)
+            print(dataframe.shape)
 
-            # Create the Excel file
-            excel_file = 'report/billing_excel_record.xlsx'
-            df.to_excel(excel_file, index=False)
+            header = "XYZ Report"
+            footer = "Overall Amount: {total_sum}".format(total_sum=dataframe["total_amount"].sum())
+            writer = pd.ExcelWriter("report/billing_excel_record.xlsx", engine='xlsxwriter')
+            dataframe.to_excel(writer, sheet_name="DATA_SHEET", header=True, index=False, startrow=1)
+            workbook = writer.book
+            worksheet = writer.sheets['DATA_SHEET']
+
+            header_format1 = workbook.add_format({'bold': True,
+                                                  'align': 'center',
+                                                  'valign': 'vcenter',
+                                                  'fg_color': '#D7E4BC',
+                                                  'text_wrap': '1',
+                                                  'font_size': '18',
+                                                  'border': 1})
+
+            footer_format1 = workbook.add_format({'bold': True,
+                                                  'align': 'center',
+                                                  'valign': 'vcenter',
+                                                  'text_wrap': '1',
+                                                  'font_size': '10',
+                                                  'border': 1})
+
+            worksheet.merge_range(0, 0, 0, len(dataframe.columns) - 1, header, header_format1)
+            worksheet.merge_range(dataframe.shape[0] + 2,
+                                  0,
+                                  dataframe.shape[0] + 2,
+                                  len(dataframe.columns) - 1,
+                                  footer,
+                                  footer_format1)
+            writer.close()
 
             # Return the Excel file as a FileResponse
-            return FileResponse(excel_file,
-                                media_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-                                filename='report/billing_excel_record.xlsx')
+            return FileResponse("report/billing_excel_record.xlsx", filename="billing_excel_record.xlsx")
         except Exception as err:
             logger.error(ExcelHandlerException.EX018.format(error=str(err)))
